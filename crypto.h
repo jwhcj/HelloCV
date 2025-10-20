f885a9fc5aff7ec1bc1139c56ca9a23754f215d5
jwhcj <13613936870@163.com>
2025-10-20 17:21:12 +0800

    Create crypto.h

---
+++ crypto.h
@@ -0,0 +1,8 @@
+#include<string>
+using namespace std;
+
+class crypto{
+public:
+static string ceasarencrypto(const string text,int key);
+static string ceasardecrypto(const string text,int key);
+};
