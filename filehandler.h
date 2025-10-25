55cc836c5024ce5327675ed14f7506e6bcd34b7a
jwhcj <13613936870@163.com>
2025-10-25 19:37:02 +0800

    Delete filehandler.h

--- filehandler.h
+++
@@ -1,8 +0,0 @@
-#include<string>
-using namespace std;
-
-class filehandler{
-public:
-     static string readfile(const string filename);
-     static void writefile(const string filename,const string content);
-};
