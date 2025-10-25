097c93aa78b67bebbeba63ebe21aa3dbede9a547
jwhcj <13613936870@163.com>
2025-10-25 19:36:34 +0800

    Delete crypto.h

--- crypto.h
+++
@@ -1,9 +0,0 @@
-
-#include<string>
-using namespace std;
-
-class crypto{
-      public:
-      static string ceasarencrypto(const string text,int key);
-      static string ceasardecrypto(const string text,int key);
-};
